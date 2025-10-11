e49ecc52683f29230a7cee72fa945cbb55f32353
Brian Jipson <bmj2728@gmail.com>
2025-10-11 15:22:28 -0400

    Generate protoc and gRPC definitions for `filesystem` and `filelister` modules. Add initial implementation of `ngfs` package for filesystem operations and gRPC service scaffolding.

--- shared/pkg/ngfs/ngfs.go
+++ shared/pkg/ngfs/ngfs.go
@@ -1,1 +1,31 @@
+// Package ngfs provides wrappers for various file system functions used by the host file system service
 package ngfs
+
+// The host service will receive a request over gRPC.
+// We then need to validate that the requestor has permission to perform the action.
+// We also need to validate authority to operator on the file/directory.
+// This should be achieved elsewhere.
+// This package will be used if we need to specifically override functions or provide helpers for more complex
+// operations.
+// for instance, consider a ReadDir example:
+//// BasicReadDir reads the contents of the directory specified by the given path and
+//func BasicReadDir(path string) ([]os.DirEntry, error) {
+//	return os.ReadDir(path)
+//}
+//
+//// BetterReadDir opens a new root to help protect against path traversal attacks, then read the directory.
+//func BetterReadDir(path string) ([]os.DirEntry, error) {
+//	r, err := os.OpenRoot(path)
+//	if err != nil {
+//		hclog.Default().Error("Failed to open root", logger.KeyError, err)
+//		return nil, err
+//	}
+//	defer func(r *os.Root) {
+//		err := r.Close()
+//		if err != nil {
+//			hclog.Default().Error("Failed to close root", logger.KeyError, err)
+//		}
+//	}(r)
+//	// Read the directory, returning the slice of DirEntry and an error, close the root
+//	return fs.ReadDir(r.FS(), ".")
+//}
